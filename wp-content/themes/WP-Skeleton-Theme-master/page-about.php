42b6e7fe314c6105083c7df9c64a876cf5fce345
Nanjaa <steph.o-91@hotmail.com>
2015-10-19 15:57:29 -0500

    Did the basics for the about page

--- wp-content/themes/WP-Skeleton-Theme-master/page-about.php
+++ wp-content/themes/WP-Skeleton-Theme-master/page-about.php
@@ -1,10 +1,18 @@
 <?php 
+	// header
 	get_header();
 	get_template_part('menu', 'index');
-	echo('hello there world!');
-
+	// profile pic
 	$image = get_field('profile_pic');
 	if( !empty($image) ): ?>
 		<img class="circledImage" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-	get_footer();
 <?php endif; ?>
+<?php
+	// content
+	while ( have_posts() ) : the_post();
+		the_content();
+	endwhile; 
+?>
+<?php
+	get_footer();
+?>
