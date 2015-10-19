c936c1dc1ec75c91297b8d4346559089ca6bed7a
Nanjaa <steph.o-91@hotmail.com>
2015-10-19 17:26:04 -0500

    Clicking on 'Recipes' will now display a page with your recipe posts

--- wp-content/themes/WP-Skeleton-Theme-master/sidebar.php
+++ wp-content/themes/WP-Skeleton-Theme-master/sidebar.php
@@ -4,11 +4,17 @@
  * @subpackage WP-Skeleton
  */
 ?>
-    <div class="one-third column omega" id="side">
-        <div class="sidebar"> <!--  the Sidebar -->
-            <?php if ( is_active_sidebar( 'right-sidebar' ) ) : ?> <?php dynamic_sidebar( 'right-sidebar' ); ?>
-            <?php else : ?><p>You need to drag a widget into your sidebar in the WordPress Admin</p>
-	        <?php endif; ?>
-	        <p> Hello world!</p>
-       </div>
-    </div>
+	<div class="one-third column omega" id="side">
+		<div class="sidebar"> <!--  the Sidebar -->
+			<!-- Social Media -->
+			<div class="social">
+				<p>Keep in Touch</p>
+				<p>social media icons go here</p>
+			</div>
+			<!-- all the rest -->
+			<?php if ( is_active_sidebar( 'right-sidebar' ) ) : ?> <?php dynamic_sidebar( 'right-sidebar' ); ?>
+			<p> Hello world!</p>
+			<?php else : ?><p>You need to drag a widget into your sidebar in the WordPress Admin</p>
+			<?php endif; ?>
+		</div>
+	</div>
