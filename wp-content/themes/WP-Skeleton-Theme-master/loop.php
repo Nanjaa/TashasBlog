b2f68c9f76297df129d12de0c6aba454eb55a67b
Nanjaa <steph.o-91@hotmail.com>
2015-11-04 13:08:25 -0600

    Changed the way the preview text worked.

--- wp-content/themes/WP-Skeleton-Theme-master/loop.php
+++ wp-content/themes/WP-Skeleton-Theme-master/loop.php
@@ -17,12 +17,10 @@
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 							<h3 class="postTitle<?php if(!in_category( 'myadventures' )) { echo ' recipeTitle'; }; ?>"><?php the_title(); ?></h3>
 						</a>
-						<?php if(in_category( 'myadventures')) { the_content(); } else { the_post_thumbnail(); }; ?>
 						<div class="preview">
-							<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac ultrices massa. In nec mauris tempus, imperdiet urna ac, blandit urna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi ornare, elit eu blandit consectetur, mi mi ullamcorper lectus, eu porttitor nisl nulla non augue. Sed at dolor leo. Quisque laoreet feugiat lorem. Vivamus sollicitudin nulla in mauris finibus, a bibendum felis commodo. Donec consectetur nisi elit, non sodales risus commodo sed. Integer imperdiet, sem at condimentum imperdiet, purus turpis semper nisl, sit amet facilisis velit erat et erat. Nullam eu libero quam. Nulla dictum tellus sed nibh efficitur sodales. Nunc pretium magna et nisl ultrices fermentum. Nam vitae facilisis lacus. Curabitur mi nisi, sollicitudin a finibus vitae, lacinia vel arcu. Phasellus eu tempor nulla. Sed vitae interdum magna.
-
-Ut rhoncus ante nec condimentum cursus. Mauris eu diam sodales, dignissim lorem at, vulputate orci. Suspendisse ultrices tincidunt libero, a viverra nulla cursus at. Integer a nibh vitae justo blandit volutpat ac vel risus. Morbi eu rhoncus sapien, et finibus sapien. Duis velit purus, volutpat laoreet ex at, dictum ornare orci. In vitae quam id est commodo lobortis pulvinar non tellus. Maecenas eleifend erat sed ullamcorper pharetra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin lobortis tortor et libero ornare posuere. Maecenas consectetur mauris ipsum, vitae vulputate dolor pretium ac.</p>
+							<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 						</div>
+						<?php if(in_category( 'myadventures')) { the_content(); } else { the_post_thumbnail(); }; ?>
 					</div>
 					<div class="test">
 						<?php echo '<a class="readMore" href="'; the_permalink(); echo '" title="'; the_title_attribute(); echo '">Read More...</a>'; ?>
--- wp-content/themes/WP-Skeleton-Theme-master/script.js
+++ wp-content/themes/WP-Skeleton-Theme-master/script.js
@@ -4,7 +4,10 @@ $j(document).ready(function() {
 
 	// Hover over the post titles, and have the 
 	$j('.postTitle').on('mouseenter', function() {
-		console.log('hello there');
+		$j(this).parent().next().fadeIn(1000);
+	});
+	$j('.postTitle').on('mouseout', function() {
+		$j('.preview').fadeOut(1000);
 	});
 
 });
