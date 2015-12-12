9bc57901f7b39a31318099fc0ee020687c04a23d
Nanjaa <steph.o-91@hotmail.com>
2015-12-11 21:31:53 -0600

    Edited the menu at the top

--- wp-content/themes/WP-Skeleton-Theme-master/menu.php
+++ wp-content/themes/WP-Skeleton-Theme-master/menu.php
@@ -16,22 +16,25 @@
 
     <div> 
 
-        <div class="center">
-
+        <div class="center menuContainer">
             <!-- The Title -->
-            <a href="<?php echo home_url(); //make logo a home link?>">
+            <a href="<?php echo home_url(); //make logo a home link ?>">
                 <!-- <h1 class="logo">Tasha's</h1> -->
                 <img class="inline" src="/wp-content/themes/WP-Skeleton-Theme-master/logo.png">
-                <h1 class="inline"><?php echo get_field('website_name/title', 5); ?></h1>
             </a>
-            <!-- The menu -->
-            <div class="navigation">
-                <a class="menuLink" href="<?php echo home_url(); ?>">Home</a>|
-                <a class="menuLink" href="/about/">About Me</a>|
-                <a class="menuLink" href="/category/myadventures/">My Adventures</a>|
-                <a class="menuLink" href="/category/diy/">DIY</a>|
-                <a class="menuLink" href="/category/recipes/">Recipes</a>
-            </div>
+            <nav class="inline">
+                <a href="<?php echo home_url(); ?>">
+                    <h1><?php echo get_field('website_name/title', 5); ?></h1>
+                </a>
+                <!-- The menu -->
+                <div class="navigation">
+                    <a class="menuLink" href="<?php echo home_url(); ?>">Home</a>|
+                    <a class="menuLink" href="/about/">About Me</a>|
+                    <a class="menuLink" href="/category/myadventures/">My Adventures</a>|
+                    <a class="menuLink" href="/category/diy/">DIY</a>|
+                    <a class="menuLink" href="/category/recipes/">Recipes</a>
+                </div>
+            </nav>
         </div>
 
     </div> 
