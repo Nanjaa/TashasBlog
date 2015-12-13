e18c25e1875046861a246da6eff9a4a0164a8eac
Nanjaa <steph.o-91@hotmail.com>
2015-12-12 21:45:00 -0600

    Changed the way the plugins were displayed using their shortcode

--- wp-content/themes/WP-Skeleton-Theme-master/single.php
+++ wp-content/themes/WP-Skeleton-Theme-master/single.php
@@ -11,11 +11,19 @@
 		<div class="two-thirds column alpha">
 			<div class="main"> 
 				<?php							
-					while(have_posts()) : the_post();
+				while(have_posts()) : the_post();
 				?>
-				<h2 class="fullpageTitle"><?php echo get_the_title(); ?></h2>
-				<?php the_content();
+					<h2 class="fullpageTitle"><?php echo get_the_title(); ?></h2>
+					<?php the_content();
+					?>
 
+					<div id="nav-below">
+						<div class="nav-previous inline"><?php if(previous_post_link()) { previous_post_link('Previous Post'); } else { echo '<p class="pageUnavailable">No Previous Posts</p>'; } ?></div>
+						<div class="nav-next inline"><?php if(next_post_link()) { next_post_link('Next Post'); } else { echo '<p class="pageUnavailable">No Future Posts</p>'; } ?></div>
+					</div><!-- #nav-below -->
+				<?php 
+					echo do_shortcode('[shareaholic app="share_buttons"]');
+					echo do_shortcode('[fbcomments]');
 				endwhile;
 				?>
 			</div>  <!-- End Main -->
