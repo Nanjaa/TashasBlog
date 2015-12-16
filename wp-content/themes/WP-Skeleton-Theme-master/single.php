85cf08023a261b3c1ac270c3243df50efe8e1171
Nanjaa <steph.o-91@hotmail.com>
2015-12-15 22:45:06 -0600

    Updated the formatting of the previous/next navigation on each of the posts

--- wp-content/themes/WP-Skeleton-Theme-master/single.php
+++ wp-content/themes/WP-Skeleton-Theme-master/single.php
@@ -19,11 +19,40 @@
 
 					<div id="nav-below">
 						<div class="nav-previous previous-post inline">
-							<?php previous_post_link(); echo '<p class="pageUnavailable">Previous Post</p>'; ?>
+							<?php 
+								$prev_link = get_previous_post();
+								if(!empty( $prev_link )):
+									$prevy = $prev_link->post_title;
+								if(strlen($prevy) > 45) {
+									$prev_short = substr($prevy,0,45).'...';
+								}
+								else {
+									$prev_short = $prevy;
+								}
+								endif;
+							?>
+							<?php 
+								previous_post_link('%link', $prev_short );
+								echo '<p class="pageUnavailable">Previous Post</p>'; 
+							?>
 						</div>
 						<div class="nav-next next-post inline">
-							<?php next_post_link(); echo '<p class="pageUnavailable">Next Post</p>'; ?>
-						</div>
+							<?php 
+								$next_link = get_next_post();
+								if(!empty( $next_link )):
+									$nexty = $next_link->post_title;
+								if(strlen($nexty) > 45) {
+									$next_short = substr($nexty,0,45).'...';
+								}
+								else {
+									$next_short = $nexty;
+								}
+								endif;
+							?>
+							<?php 
+								next_post_link('%link', $next_short );
+								echo '<p class="pageUnavailable">Previous Post</p>'; 
+							?>						</div>
 					</div><!-- #nav-below -->
 				<?php 
 					echo do_shortcode('[shareaholic app="share_buttons"]');
