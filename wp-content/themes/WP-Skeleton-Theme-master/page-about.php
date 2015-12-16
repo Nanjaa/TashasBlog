120d838dcf40494915ffd9bb7ecaca7eca89c3a2
Nanjaa <steph.o-91@hotmail.com>
2015-12-15 22:07:20 -0600

    Worked on the responsiveness a bit more

--- wp-content/themes/WP-Skeleton-Theme-master/page-about.php
+++ wp-content/themes/WP-Skeleton-Theme-master/page-about.php
@@ -6,21 +6,22 @@
 
 	<div class="content">
 		<div class="two-thirds column alpha">
-			<div class="main"> 
+			<div class="main">
+				<div class="aboutTitle">
 				<?php 
-				// profile pic
-				$image = get_field('profile_pic');
-				$size = 'tn300';
-				$image_sized = $image['sizes'][$size];
-				if( !empty($image) ): ?>
-					<img class="circledImage" src="<?php echo $image_sized; ?>" alt="<?php echo $image['alt']; ?>" />
-				<?php endif; ?>
-			<?php
+					// profile pic
+					$image = get_field('profile_pic');
+					$size = 'tn300';
+					$image_sized = $image['sizes'][$size];
+					if( !empty($image) ): ?>
+						<img class="circledImage" src="<?php echo $image_sized; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				<?php
 				// this is the var for the headline 
 				$headline = get_field('headline');
 				// content
 				while ( have_posts() ) : the_post();
-					echo '<h1 class="aboutHeadline">'; echo $headline; echo '</h1>';
+					echo '<h1 class="aboutHeadline">'; echo $headline; echo '</h1></div>';
 					the_content();
 				endwhile; 
 			?>
